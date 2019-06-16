c7658a441edd31ea5188bab3c2d0d71642e86b84
K-D-Junha <junha179@naver.com>
2019-06-17 00:52:25 +0900

    190617_0052
    포스팅기능 보완
    *삭제, 수정 추가
    (삭제는 일단 아이콘 누르면 바로삭제하는걸로,
    포스팅 정렬은 업데이트 시간 역순으로)
    +포스팅에서 작성자 썸네일, id 나오게 수정함

--- PictoryProject/Posting/views.py
+++ PictoryProject/Posting/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Comment
 from django.contrib.auth.models import User
-from .models import Post,Comment
+import sys
+sys.path.append("..")   #상위 폴더 import는 이렇게 한다
+from Member.models import Profile 
+
 
 # Create your views here.
 
 def posting(request):
-    postings=Post.objects.all().order_by('-pub_date')
-    return render(request, 'Posting/My_posting_list.html',{'postings':postings})
+    postings=Post.objects.filter(user_id=request.user.id).order_by('-pub_date') #현재 유저의 포스팅만 가져오기
+    #postings=Post.objects.all().order_by('-pub_date')  #이건 타임라인에서 쓸 것
+    profile=Profile.objects.get(owner_id=request.user.id)
+    return render(request, 'Posting/My_posting_list.html',{'postings':postings, 'profile':profile}) #dictionary 여러개 보내는 거 되나? 하나안엔 되네
 
 def new(request):
     return render(request,'Posting/new.html')
@@ -24,4 +29,26 @@ def create(request):
     post.user=get_object_or_404(User,pk=request.POST.get('user_id',False))
     post.save()
 
-    return redirect('/posting/' + str(post.id))
+    return redirect('/posting/')
+
+def delete(request, post_id):
+    post=get_object_or_404(Post, pk=post_id)
+    post.delete()
+
+    return redirect('/posting/')
+
+
+def edit(request, post_id):
+    old_post = get_object_or_404(Post,pk=post_id)
+    return render(request, "Posting/edit.html", {"old" : old_post})
+
+def update(request,post_id):
+    post=get_object_or_404(Post,pk=post_id)
+    post.title = request.POST.get('title',False)
+    post.description = request.POST.get('des',False)
+    post.pub_date = timezone.datetime.now()
+    if(request.POST.get('image')!=''):
+        post.image=request.FILES['image']
+    post.save()
+
+    return redirect('/posting/')
